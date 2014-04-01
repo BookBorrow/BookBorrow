16dd1896cb2225a664c71c03ea1a2cbc0357ebf6
alexpatriquin <alexpatriquin@gmail.com>
2014-04-01 13:08:33 -0400

    add users and book controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
 
   devise_for :users 
   devise_scope :user do
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
