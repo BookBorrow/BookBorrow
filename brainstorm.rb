e83f94a64860b9b88c4a64296260c327707fd38d
Michael Prouty <michael.prouty@gmail.com>
2014-04-01 10:11:34 -0400

    Initial commit

---
+++ brainstorm.rb
@@ -0,0 +1,22 @@
+# Sign in with FB.
+
+
+# Profile Picture
+
+# Borrowing:
+============
+# <a>Friend<a> => [ 
+      [ <a>book1<image></a>, due_date<color changes>],
+      [ <a>book2<image></a>, due_date<color changes>]
+    ]
+
+# Borrowers:
+============
+
+# <a>Friend<a> => [
+      [<a>book1<image></a>, <a>book2<image></a>]
+      [<a>book1<image></a>, <a>book2<image></a>],
+]
+
+
+
---
+++ goodreads_client.rb
@@ -0,0 +1,4 @@
+client = Goodreads::Client.new(:api_key => 'Gl6twTsRxltWfQcx9FsjQ', :api_secret => 'xDWj5dPQIz99eYmgOAzMpYcPo1wywOWrMzRJJy5MSo')
+client = Goodreads.new(:api_key => 'Gl6twTsRxltWfQcx9FsjQ')
+
+
