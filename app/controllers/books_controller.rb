50c0382b6392c38d3461217ceb20b2f2d3ba6717
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-02 12:12:22 -0400

    implemented json objects for global books list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,28 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.text_search(params[:query]).page(params[:page]).per(10)
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json  {render json: Book.text_search(params[:query])}
+    end
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
+  def find
+
+    render json: Book.text_search(params[:query])
+  end
+
 
   private
-    def set_book
-      @book = Book.find(params[:id])
-    end
+  def set_book
+    @book = Book.find(params[:id])
+  end
 
-    def book_params
-      params.require(:book).permit(:title, :description, :isbn, :author, :cover_url)
-    end
+  def book_params
+    params.require(:book).permit(:title, :description, :isbn, :author, :cover_url)
+  end
 end
