fe74335f7483e53c71dbd584fadd3116243b5631
alexpatriquin <alexpatriquin@gmail.com>
2014-04-02 14:29:50 -0400

    style form links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
   def avatar_for(user)
-    default_url = "#{root_url}images/guest.png"
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     pixels = 100
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    default_url = "#{root_url}images/guest.png"
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{pixels}"
   end
 
