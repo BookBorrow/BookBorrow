2c97322d7eb69674bd8d3384d8e57dd6425d07a6
Michael Prouty <michael.prouty@gmail.com>
2014-04-02 17:44:06 -0400

    link up new borrow form

--- spec/models/borrow_spec.rb
+++ spec/models/borrow_spec.rb
@@ -1,13 +1,4 @@
 require 'spec_helper'
 
 describe Borrow do
-  describe "validations" do
-    it "should have many borrows" do
-      @book = create(:book)
-      @user = create(:user)
-      @book.user_books.create(:user => @user)
-      @user_book = @book.user_books.first
-
-    end
-  end
 end
