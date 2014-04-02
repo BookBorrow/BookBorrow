091850820ef6b23f49bd7f574980a8ea4734eaff
alexpatriquin <alexpatriquin@gmail.com>
2014-04-02 13:38:28 -0400

    gravatar support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
 
+  def avatar_for(user)
+    default_url = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    pixels = 100
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{pixels}"
+  end
+
 end
