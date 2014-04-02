368efa591b941b2000f295c184450f4a59fe4473
alexpatriquin <alexpatriquin@gmail.com>
2014-04-02 15:32:23 -0400

    add profile public default

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
+  before_save :set_public
 
   def id_is? id
     self.id == id
+  end
+
+  protected
+
+  def set_public
+    self.public = true if self.public.nil?
   end
 
 end
