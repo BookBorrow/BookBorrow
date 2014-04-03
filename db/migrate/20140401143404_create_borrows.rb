2538146aafc04b8816ed851af88770d4bd16e0f1
alexpatriquin <alexpatriquin@gmail.com>
2014-04-03 10:18:33 -0400

    update postmark api keys

--- db/migrate/20140401143404_create_borrows.rb
+++ db/migrate/20140401143404_create_borrows.rb
@@ -3,7 +3,7 @@ class CreateBorrows < ActiveRecord::Migration
     create_table :borrows do |t|
       t.integer :user_book_id
       t.integer :user_id
-      t.date :borrow_date
+      t.date    :borrow_date
       t.integer :duration_in_days
 
       t.timestamps
