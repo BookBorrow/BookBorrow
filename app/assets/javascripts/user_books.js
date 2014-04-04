846fb74eba7989a9dffee477d709a7313e03eb83
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-04 15:51:18 -0400

    Removed notworking modal

--- app/assets/javascripts/user_books.js
+++ app/assets/javascripts/user_books.js
@@ -1,14 +1,12 @@
-$(function(){
-  $('#new_borrow_submit').click(function(){
-    $form = $("form#new_borrow");
-    $form.submit();
-  });
+$(function() {
+    $('#new_borrow_submit').click(function() {
+        $form = $("form#new_borrow");
+        $form.submit();
+    });
 
-  $('.loanable-button').click(function(e){
-    //e.preventDefault();
-    $('#myModal').modal();
-  });
+    $('.loanable-button').click(function(e) {
+        //e.preventDefault();
+        // $('#myModal').modal();
+    });
 
 });
-
-
