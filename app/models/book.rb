5905cea107913d6c8e7cfa9e5105ad964151f1a2
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-04-07 11:30:55 -0400

    100% of book tests coverage

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,6 @@ class Book < ActiveRecord::Base
   has_many :users, :through => :user_books
   before_validation :normalize_isbn, :on => :create
   validates_uniqueness_of :isbn
-  validates_length_of :isbn, :minimum => 9
 
   include PgSearch
   pg_search_scope :search, against: [:title, :description, :isbn, :author, :cover_url], using: {tsearch: {dictionary: "english"}}
@@ -28,21 +27,22 @@ class Book < ActiveRecord::Base
   	end
   end
 
-  def title_from_google=(title)
-    search = GoogleBooks.search("title: #{title}", {:count => 1, :api_key => "AIzaSyCvOuvO_hQS_ZULj8Q4vIBttWefh6kv8zY"})
-    if search.first
-      record = search.first
-      self.title = record.title
-      self.description = record.description
-      self.author = record.authors_array[0]||record.authors||"unknown"
-      self.isbn = record.isbn
-      self.cover_url = record.image_link
-      self.categories = record.categories
-      self.ratings_count = record.ratings_count,
-      self.average_rating = record.average_rating,
-      self.page_count = record.page_count
-    end
-  end
+# NOT NECESSARY
+  # def title_from_google=(title)
+  #   search = GoogleBooks.search("title: #{title}", {:count => 1, :api_key => "AIzaSyCvOuvO_hQS_ZULj8Q4vIBttWefh6kv8zY"})
+  #   if search.first
+  #     record = search.first
+  #     self.title = record.title
+  #     self.description = record.description
+  #     self.author = record.authors_array[0]||record.authors||"unknown"
+  #     self.isbn = record.isbn
+  #     self.cover_url = record.image_link
+  #     self.categories = record.categories
+  #     self.ratings_count = record.ratings_count,
+  #     self.average_rating = record.average_rating,
+  #     self.page_count = record.page_count
+  #   end
+  # end
 
   def self.text_search(query)
     if query.present?
@@ -58,6 +58,6 @@ private
   end
 
   def self.strip_isbn(isbn)
-    isbn.to_s.gsub(/\D/,"")
+    isbn.to_s.gsub(/\D/,"").to_f
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -17,14 +17,40 @@ describe Book do
       expect(@book2).not_to be_valid
     end
 
+    it "validates numericality for isbn with non-numbers" do
+      @book = Book.new(:isbn => "1ABC234ABC")
+      expect(@book).not_to be_valid
+    end
+
     it "validates length <= 9 for isbn" do
       @book = Book.new(:isbn => "1234")
       expect(@book).not_to be_valid
     end
 
-    it "validates length >= 9 for isbn" do
+    it "validates length <= 9 for isbn" do
       @book = Book.new(:isbn => "123456789")
       expect(@book).to be_valid
+    end
+
+
+    it "creates a new book from Google using isbn" do
+      Book.create_from_google("0241950430")
+      expect(Book.last.title).to eq("Catcher in the Rye")
+    end
+
+    it "creates a new book from Google using title" do
+      Book.create_from_google("Everyone Poops", mode = "title")
+      expect(Book.last.title).to eq("Everyone Poops")
+    end
+
+    it "returns results for a search of an existing book" do
+      @book = create(:book)
+      expect(Book.text_search("Catcher")).to eq(Book.last)
+    end
+
+    it "returns all results for a search with no query" do
+      @book = create(:book)
+      expect(Book.text_search("")).to be_a(Array)
     end
 
   end
