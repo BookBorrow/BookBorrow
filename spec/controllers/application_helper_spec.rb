320c68c8f033fd99da9106b1f460797c3335f280
Kevin Chang <kevin.w.chang@gmail.com>
2014-04-21 16:04:29 -0400

    Working mini-gravatar test

--- spec/controllers/application_helper_spec.rb
+++ spec/controllers/application_helper_spec.rb
@@ -2,13 +2,22 @@ require 'spec_helper'
 require 'application_helper'
 describe 'Application Helper' do
 
-let(:dummy_class) { Class.new { include ApplicationHelper } }
-    it 'should know what its gravatar url is' do
-      user =  create(:user)
-      user.update(email: "test@test.com")
-      dummy_class.any_instance.stub(:root_url).and_return("http://localhost:3000")
+  let(:dummy_class) { Class.new { include ApplicationHelper } }
+  it 'should know what its gravatar url is' do
+    user =  create(:user)
+    user.update(email: "test@test.com")
+    dummy_class.any_instance.stub(:root_url).and_return("http://localhost:3000")
 
-      expect(dummy_class.new.avatar_for(user)).to eq("http://gravatar.com/avatar/b642b4217b34b1e8d3bd915fc65c4452.png?s=100")
-    end
+    expect(dummy_class.new.avatar_for(user)).to eq("http://gravatar.com/avatar/b642b4217b34b1e8d3bd915fc65c4452.png?s=100")
+  end
+
+  let(:dummy_class) { Class.new { include ApplicationHelper } }
+  it 'should know what its mini-gravatar url is' do
+    user =  create(:user)
+    user.update(email: "test@test.com")
+    dummy_class.any_instance.stub(:root_url).and_return("http://localhost:3000")
+
+    expect(dummy_class.new.mini_avatar(user)).to eq("http://gravatar.com/avatar/b642b4217b34b1e8d3bd915fc65c4452.png?s=50")
+  end
 
 end
